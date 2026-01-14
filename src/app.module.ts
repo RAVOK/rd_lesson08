81393487e96ef284bd8fc28a5507937ade2be588
O.Rogov <Alexandr.Rogov@areon.ua>
2026-01-14 19:18:54 +0200

    add README

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // доступний у всіх модулях
-      envFilePath: `.env.${process.env.NODE_ENV || 'local'}`, // вибір файлу залежно від NODE_ENV
+      isGlobal: true,
+      envFilePath: join(__dirname, '..', 'config', `.env.${process.env.NODE_ENV || 'local'}`),
     }),
     UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-
-
 
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // отримуємо доступ до ConfigService
-  const configService = app.get(ConfigService);
-  // читаємо порт із конфігурації
-  const port = configService.get<number>('PORT') ?? 7000;
+  const configService = app.get(ConfigService);             // отримуємо доступ до ConfigService  
+  const port = configService.get<number>('PORT') ?? 7000;   // читаємо порт із конфігурації
 
   await app.listen(port);
+  
   console.log(`Application is running on: http://localhost:${port}`);
-
 }
 bootstrap();
